af3a7c04f599c2506373637f30cbfc37e9ca970b
Muhammad Mustafa <mustafashoaib001@gmail.com>
2019-02-08 01:52:42 +0500

    Adding more functionality like improving search items and clicking to refresh the current video with the selected video from the list

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,16 +3,22 @@ import React, { Component } from 'react';
 class SearchBar extends Component{
 	constructor(props){
 		super(props);
-		this.state ={ term: 'default value' };
+		this.state ={ term: '' };
 	}
 	render(){
 		return (
-			<div>
+			<div className="search-bar">
 			<input
 				value={this.state.term}
-			 onChange={input=> this.setState({term: input.target.value})} />
+			 onChange={input=> this.onInputChange(input)} />
 			</div>
 			);
+	}
+
+	onInputChange(input){
+		const term = input.target.value;
+		this.setState({term});
+		this.props.onSearchTermChange(term);
 	}
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React, { Component} from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 
 import SearchBar from './components/search_bar';
 
 import YTAPISearch from 'youtube-api-search';
 
 import VideoList from './components/video_list';
+
+import VideoDetail from './components/video_details';
 
 const YOUTUBE_API_KEY = "AIzaSyC5lsf4aCFgkFruPM2n9JDIyzVspcMijIs";
 
@@ -14,22 +17,35 @@ const YOUTUBE_API_KEY = "AIzaSyC5lsf4aCFgkFruPM2n9JDIyzVspcMijIs";
 class App extends Component{
 	constructor(props){
 		super(props);
-		this.state = { videos:[] };
-		this.youtubeSearch();
+		this.state = {
+			videos:[],
+			selectedVideo: null
+		 };
+		this.youtubeSearch("surfboard");
 	}
 
-	youtubeSearch(){
+	youtubeSearch(term){
 		console.log("Youtube search starts.......");
 		YTAPISearch({
 			key: YOUTUBE_API_KEY,
-			term: 'surfboard'
-		},videos => this.setState({videos}));
+			term: term,
+		},videos => this.setState({
+			videos:videos,
+			selectedVideo: videos[0]
+		}));
 	}
 	render(){
+		const videoSearch = _.debounce(term=>{this.youtubeSearch(term)},300)
 		return (
 			<div>
-				<SearchBar />
-				<VideoList videos={this.state.videos} />
+				<SearchBar
+				 onSearchTermChange={videoSearch}/>
+
+				<VideoDetail video={this.state.selectedVideo}/>
+
+				<VideoList
+				onVideoSelect={selectedVideo=>this.setState({selectedVideo})}
+				videos={this.state.videos} />
 			</div>
 			);
 	}
@@ -38,4 +54,3 @@ class App extends Component{
 
 
 ReactDOM.render(<App/>,document.querySelector(".container"));
-
